99b9e7e42361631fbc98c4239b3ff448c96698e2
margaretzny <margaret.zhuny@gmail.com>
2025-02-22 18:51:03 -0500

    modfied BondPrice_Z_File

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,16 +1,21 @@
+import sys
+sys.path.append("/Users/zny/Desktop/Git/zny/BondPrice_Z_File.py") 
 
 
-def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
-    
-﻿def getBondPrice_Z(face, couponRate, times, yc):
-    <Your work using zip>
-    return(bondPrice)
+import BondPrice_Z_File 
+
+def test_getBondPrice_Z():
+    yc = [.010,.015,.020,.025,.030]
+    times=[1,1.5,3,4,7]
+    face = 2000000
+    couponRate = .04
+
+    x = BondPrice_Z_File.getBondPrice_Z(face, couponRate, times, yc)
+    return x
 
 
-# Test values
-﻿
-yc = [.010,.015,.020,.025,.030]
-times=[1,1.5,3,4,7]
-face = 2000000
-couponRate = .04
+
+
+print(test_getBondPrice_Z())
+
+
